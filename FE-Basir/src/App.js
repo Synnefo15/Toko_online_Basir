ab9c8ac9b3b245b564a81f7720221f45e29c3cda
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-07-01 20:26:37 +0700

    init repo

---
+++ FE-Basir/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Bekasmu</a>
+      </header>
+      <main>
+        List Product
+      </main>
+    </div>
+  );
+}
+
+export default App;
