0c616751ab0965188fef4f55e3a048796938def3
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-07-01 21:29:25 +0700

    product card

---
+++ FE-Basir/src/data.js
@@ -0,0 +1,42 @@
+const data ={
+    products:[
+        {
+            name:'Nike Slim Shirt',
+            slug:'nike-slim-shirt',
+            category:'Shirt',
+            image:'/images/p1.jpg',
+            price : 120,
+            countInStock:10,
+            brand:'nike',
+            rating:4.5,
+            numReviews:10,
+            description : "anti air tahan banting"
+        },
+        {
+            name:'Nike Slim pant',
+            slug:'nike-slim-pant',
+            category:'Pant',
+            image:'/images/p2.jpg',
+            price : 220,
+            countInStock:30,
+            brand:'nike',
+            rating:4.5,
+            numReviews:10,
+            description : "anti air tahan banting"
+        },
+        {
+            name:'Adidas fit Shirt',
+            slug:'nike-fit-shirt',
+            category:'Shirt',
+            image:'/images/p3.jpg',
+            price : 130,
+            countInStock:60,
+            brand:'adidas',
+            rating:3.5,
+            numReviews:10,
+            description : "anti air tahan banting"
+        },
+    ]
+}
+
+export default data
---
+++ README.md
@@ -0,0 +1,8 @@
+# Toko Bekas
+
+## Tech & platform that used
+1. React JS
+2. Mongo DB
+3. Express
+4. React bootstrap
+
