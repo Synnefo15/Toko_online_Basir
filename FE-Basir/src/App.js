0ec4674d855015e56fed5d609a036c38f3057d52
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-07-05 13:44:25 +0700

    18 signIn Page

--- FE-Basir/src/App.js
+++ FE-Basir/src/App.js
@@ -7,6 +7,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SignInScreen from './screens/SignInScreen';
 
 function App() {
 	const { state } = useContext(Store);
@@ -41,6 +42,7 @@ function App() {
 							<Route path="/product/:slug" element={<ProductScreen />} />
 							<Route path="/tes" element={<Tes />} />
 							<Route path='/cart' element={<CartScreen />}/>
+							<Route path='/signin' element={<SignInScreen />}/>
 						</Routes>
 					</Container>
 				</main>
