87f72d4d48c504a3ce00a3503c8ca6a4faa9ad9b
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-07-05 16:24:14 +0700

    19-mongo db connect

--- BE-Basir/server.js
+++ BE-Basir/server.js
@@ -1,6 +1,20 @@
 import express from 'express';
 import products from './data/data_products.js';
 
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log(`koneksi sukses to mongo db`);
+	})
+	.catch((err) => {
+		console.log(err.message);
+	});
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -11,19 +25,19 @@ app.get('/api/products', (req, res) => {
 	res.send(products);
 });
 app.get('/api/products/slug/:slug', (req, res) => {
-	const product = products.find(x=>x.slug === req.params.slug)
+	const product = products.find((x) => x.slug === req.params.slug);
 	if (product) {
-		res.send(product)
-	}else{
-		res.status(404).send({message:'product not found'})
+		res.send(product);
+	} else {
+		res.status(404).send({ message: 'product not found' });
 	}
 });
 app.get('/api/products/:id', (req, res) => {
-	const product = products.find(x=>x._id === req.params.id)
+	const product = products.find((x) => x._id === req.params.id);
 	if (product) {
-		res.send(product)
-	}else{
-		res.status(404).send({message:'product not found'})
+		res.send(product);
+	} else {
+		res.status(404).send({ message: 'product not found' });
 	}
 });
 
